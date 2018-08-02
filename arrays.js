8850e558ecece62f45ebe4b0f204325c1f4c4e23
hoodielive <hoodielive@gmail.com>
2018-08-01 22:32:17 -0400

    arrays are amazing

--- arrays.js
+++ arrays.js
@@ -4,5 +4,21 @@ array1 = ['1', 1, [1,2,3], 'abcde', {name: 'loco'} ]
 
 array2 = array1[1] * array1[2][1] 
 
-console.log(array1) 
-console.log(array2)
+console.log(array1);
+
+console.log(array2);
+
+console.log(typeof(array1));
+
+function figureThisOut() { 
+    array1.filter(function(array) { 
+        if( ! array1 == typeof "object" ) {  
+            console.log('you have the wrong type for this operation') 
+        }
+        else {
+            console.log('my understanding is unfruitful') 
+        } 
+    }); 
+} 
+
+figureThisOut(); 
