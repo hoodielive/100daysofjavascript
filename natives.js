cbb164a78196c6b7d3e2916efc059d165cfe21e4
hoodielive <hoodielive@gmail.com>
2018-08-04 08:10:51 -0400

    natives - constructor functions

---
+++ natives.js
@@ -0,0 +1,24 @@
+/** Natives 
+ * Number
+ * Boolean
+ * Function
+ * Object
+ * Array
+ * RegExp
+ * Date
+ * Error
+ **/ 
+
+/** to call them Native Constructors would be more accurate 
+ * new String()
+ * new Number() 
+ * new Boolean()
+ * new Function() 
+ * new Object() 
+ * new Array() 
+ * new RegExp() 
+ * new Date() 
+ * new Error() 
+ **/ 
+
+
