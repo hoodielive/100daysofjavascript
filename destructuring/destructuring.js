0248625cffa9cd5414f076717726110e6108e34e
hoodielive <hoodielive@gmail.com>
2018-08-08 21:29:05 -0400

    destructuring objects and arrays

--- destructuring/destructuring.js
+++ destructuring/destructuring.js
@@ -1,7 +1,7 @@
-var letters = ['sugar', 'darling', 'baby'], 
-	x = letters[0],
-	y = letters[1],
-	z = letters[2]; 
+var romanticDescriptions = ['sugar', 'darling', 'baby'], 
+	x = romanticDescriptions[0],
+	y = romanticDescriptions[1],
+	z = romanticDescriptions[2]; 
 
 console.log( x, y, z ); 
 
