e1a65437d2824b816ca1498a02e723da1e50b713
hoodielive <hoodielive@gmail.com>
2018-08-09 21:37:54 -0400

    conventions on the new keyword

--- async/oop/person.js
+++ async/oop/person.js
@@ -1,7 +1,12 @@
 
+// anytime you define a constructor make it captial (convention)     
+
 const person = function(firstname) { 
-    
+
     this.firstname = firstname; 
+    
+    // no need to return anything because the option is going to be be returned because we are using the 'new' constructor 
+
 }
 
 
