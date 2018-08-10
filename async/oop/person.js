7d0b78d89318a88f37abbcbf1f2422dca3c2703a
hoodielive <hoodielive@gmail.com>
2018-08-09 21:59:52 -0400

    setName - using split

--- async/oop/person.js
+++ async/oop/person.js
@@ -14,9 +14,15 @@ Person.prototype.getBio = function() {
 } 
 
 
+Person.prototype.setName = function(fullname) {
+    const names = fullname.split(' '); 
+    this.firstname = names[0] 
+    this.lastname = names[1] 
+}
+
 const me = new Person('Larry', 'Soul', 17); 
 
-
+me.setName('Hood Man');
 console.log(me.getBio()) 
 
 const person2 = new Person('Ma', 'love', 19);
