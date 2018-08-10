bf177a7d19caad9de77246a36e03e4a7f222d564
hoodielive <hoodielive@gmail.com>
2018-08-09 21:53:07 -0400

    straigthen out fuckery

--- async/oop/hangman.js
+++ async/oop/hangman.js
@@ -1,10 +1,19 @@
+
 const Hangman = function(word, numofGuesses) { 
+
     this.word = word; 
+
     this.numofGuesses = numofGuesses; 
 } 
 
+
+// inheritance - allowing instances of the constructor is nothing but prototypal inheritance - not a true class 
+
 const instance01 = new Hangman('joe', 2); 
+
 console.log(instance01); 
 
 const instance02 = new Hangman('jee', 3); 
+
 console.log(instance02); 
+
--- async/oop/person.js
+++ async/oop/person.js
@@ -9,11 +9,15 @@ const Person = function(firstname, lastname, age) {
 }
 
 
-const me = new person('Larry', 'Soul', 17); 
+Person.prototype.getBio = function() { 
+    return `${this.firstname} is ${this.age}` // this method is shared with all instances
+} 
 
 
-console.log(me) 
+const me = new Person('Larry', 'Soul', 17); 
 
+
+console.log(me.getBio()) 
 
 const person2 = new Person('Ma', 'love', 19);
-console.log(person2)
+console.log(person2.getBio())
