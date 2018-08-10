95890c5fa45321d72205f16f6e206d99d3f0b440
hoodielive <hoodielive@gmail.com>
2018-08-09 21:39:19 -0400

    hhhhuuuuuuhhhhh

--- async/oop/person.js
+++ async/oop/person.js
@@ -1,16 +1,18 @@
 
 // anytime you define a constructor make it captial (convention)     
 
-const person = function(firstname) { 
+const person = function(firstname, lastname, age) { 
 
     this.firstname = firstname; 
+    this.lastname = lastname; 
+    this.age = age; 
     
     // no need to return anything because the option is going to be be returned because we are using the 'new' constructor 
 
 }
 
 
-const me = new person('Larry'); 
+const me = new person('Larry', 'Soul', 17); 
 
 
 console.log(me) 
